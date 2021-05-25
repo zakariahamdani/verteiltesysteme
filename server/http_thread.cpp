3f7fc4d22ab4b680382d0dbfd12c2304d76ab1cc
istzahamd <Zakaria.Hamdani@stud.h-da.de>
2021-05-25 04:32:50 +0200

    [refactoring] refactored the code in http_thread.cpp for more clarity

--- server/http_thread.cpp
+++ server/http_thread.cpp
@@ -3,186 +3,179 @@
 //
 #include "globalVariables.h"
 
+int tcpSockfd, new_socket, valread;
+struct sockaddr_in address;
+int opt = 1;
+int addrlen = sizeof(address);
+char tcpBuffer[MAXLINE] = {0};
 
-void http_server()
-{
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = {0};
+std::string content; // We save the HTML page on this variable
 
+bool send_404;
 
+std::string HTTP_method;
+std::string requestedURL;
+std::string firstSlang;
+std::string subDomain;
+std::string participantType;
+std::string participantId;
+
+void setupTcpSocket();
+void waitForConnection();
+void parseRequest();
+void constructParticipantHTML(std::string p_participantType, std::string p_participantId);
+void constructHTML404Method();
+void constructHTML404SubDomain();
+void sendRespons();
+
+void http_server() {
+
+    setupTcpSocket();
+
+    while (true) {
+
+        content = "";
+        send_404 = false;
+
+        // Here we wait for a connection to our socket
+        waitForConnection();
+
+        // Read URL from the HTTP-request and create HTML page
+        parseRequest();
+        printf("This should be the URL: %s", requestedURL.c_str());
+
+        if (HTTP_method == "GET") {
+            if (subDomain == "api") {
+                constructParticipantHTML(participantType, participantId);
+            } else {
+                constructHTML404SubDomain();
+            }
+        } else {
+            constructHTML404Method();
+        }
+        sendRespons();
+    }
+}
+
+void setupTcpSocket() {
     // Create socket and attach it to the HTTP_PORT
     // Programm closes if we can't create-connect our socket to a port
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
+    if ((tcpSockfd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
-    {
+    if (setsockopt(tcpSockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY; // Accept connections from any IP
     address.sin_port = htons(HTTP_PORT);
-    if (bind(server_fd, (struct sockaddr *)&address,
-             sizeof(address)) < 0)
-    {
+
+    if (bind(tcpSockfd, (struct sockaddr *) &address, sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
+}
 
-    // Here we wait for a connection to our socket and send a response to a HTTP request
-    while (true)
-    {
-        if (listen(server_fd, 3) < 0)
-        {
-            perror("listen");
-            exit(EXIT_FAILURE);
-        }
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                                 (socklen_t *)&addrlen)) < 0)
-        {
-            perror("accept");
-            exit(EXIT_FAILURE);
-        }
-        // At this point we already made a TCP connection with (probably) a browser
+void waitForConnection() {
+    if (listen(tcpSockfd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(tcpSockfd, (struct sockaddr *) &address,
+                             (socklen_t * ) & addrlen)) < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    // At this point we already made a TCP connection with (probably) a browser
+    // Get client (browser) request data
+    valread = read(new_socket, tcpBuffer, 1024);
+    printf("%s\n", tcpBuffer);
+}
 
-        // Get client (browser) request data
-        valread = read(new_socket, buffer, 1024);
-        printf("%s\n", buffer);
+void parseRequest() {
+    std::stringstream http_header;
+    http_header << tcpBuffer;
 
-        // Read URL from the HTTP-request and create HTML page
-        bool send_404 = false;
-        std::string content; // We save the HTML page on this variable
-        std::stringstream http_header;
-        http_header << buffer;
-        std::string http_header_word;
-        getline(http_header, http_header_word, ' ');
-        if (http_header_word == "GET")
-        {
-            getline(http_header, http_header_word, ' ');
-            http_header.str(http_header_word); // Save URL of the requested resource
-            printf("This should be the URL: %s", http_header_word.c_str());
-            getline(http_header, http_header_word, '/'); // Ignore first slash
-            getline(http_header, http_header_word, '/');
+    getline(http_header, HTTP_method, ' ');
+    getline(http_header, requestedURL, ' ');
 
-            // We accept URLs with this structure:
-            //  /api/[consumer,producer]/[id]
-            if (http_header_word == "api")
-            {
-                getline(http_header, http_header_word, '/');
-                if (http_header_word == "consumer")
-                {
-                    data_map_pointer = &consumer_data_map;
-                    getline(http_header, http_header_word, '/');
-                    // Convert ID to integer and see if we have it on list
-                    try
-                    {
-                        if (consumer_data_map.find(stoi(http_header_word)) == consumer_data_map.end())
-                        {
-                            send_404 = true;
-                            printf("ID %s wasn't found/wasn't a number", http_header_word.c_str());
-                            content.append("<h2> We couldn't find a consumer with the ID " + http_header_word + " </h2></div> </html>");
-                        }
-                        else
-                        {
-                            content.append("<h1>Consumer: " + http_header_word + "</h1>\n");
-                            auto const &pair = *data_map_pointer->find(stoi(http_header_word));
-                            content.append("<table border=\"1\" cellspacing=\"0\"><thead><tr><th></th><th>Timestamp</th><th>Value</th></tr></thead>");
-                            int index = 1;
-                            for (auto &sec_pair : pair.second)
-                            {
-                                content.append("<tr><td>" + std::to_string(index) + "</td><td>" + sec_pair.timestamp + "</td><td>" + std::to_string(sec_pair.value) + "</td></tr>\n");
-                                index++;
-                            }
-                            content.append("</table>");
-                        }
-                    }
-                    catch (std::exception &err)
-                    {
-                        std::cout << "Conversion failure: " << err.what() << std::endl; // Note: what() tells the exact error
-                    }
-                }
-                else if (http_header_word == "producer")
-                {
-                    data_map_pointer = &producer_data_map;
-                    getline(http_header, http_header_word, '/');
-                    // Convert ID to integer and see if we have it on list
-                    try
-                    {
-                        if (producer_data_map.find(stoi(http_header_word)) == producer_data_map.end())
-                        {
-                            send_404 = true;
-                            content.append("<h2> We couldn't find a producer with the ID " + http_header_word + " </h2></div> </html>");
-                            printf("ID %s wasn't found/wasn't a number", http_header_word.c_str());
-                        }
-                        else
-                        {
-                            content.append("<h1>Producer: " + http_header_word + "</h1>\n");
-                            auto const &pair = *data_map_pointer->find(stoi(http_header_word));
-                            content.append("<table border=\"1\" cellspacing=\"0\"><thead><tr><th></th><th>Timestamp</th><th>Value</th></tr></thead>");
-                            int index = 1;
-                            for (auto &sec_pair : pair.second)
-                            {
-                                content.append("<tr><td>" + std::to_string(index) + "</td><td>" + sec_pair.timestamp + "</td><td>" + std::to_string(sec_pair.value) + "</td></tr>\n");
-                                index++;
-                            }
-                            content.append("</table>");
-                        }
-                    }
-                    catch (std::exception &err)
-                    {
-                        std::cout << "Conversion failure: " << err.what() << std::endl;
-                    }
-                }
-                else
-                {
-                    send_404 = true;
-                    content.append("<h2> Sorry, we don't have a client type named " + http_header_word + " </h2></div> </html>");
-                    printf("It was %s and not consumer/producer", http_header_word.c_str());
-                }
-            }
-            else
-            {
-                send_404 = true;
-                content.append("<h2> We couldn't find a subsection named " + http_header_word + " </h2></div> </html>");
-                printf("It was %s and not api", http_header_word.c_str());
-            }
-        }
-        else
-        {
+    http_header.str(requestedURL); // Save URL of the requested resource
+
+    getline(http_header, firstSlang, '/'); // Ignore first slash
+    getline(http_header, subDomain, '/');
+    getline(http_header, participantType, '/');
+    getline(http_header, participantId, '/');
+}
+
+void constructParticipantHTML(std::string p_participantType, std::string p_participantId) {
+
+    if (p_participantType == "consumer") {
+        data_map_pointer = &consumer_data_map;
+    } else if (p_participantType == "producer") {
+        data_map_pointer = &producer_data_map;
+    } else {
+        send_404 = true;
+        content.append(
+                "<h2> Sorry, we don't have a participant type named " + p_participantType + " </h2></div> </html>");
+        printf("It was %s and not consumer/producer", p_participantType.c_str());
+        return;
+    }
+
+    try {
+        if (data_map_pointer->find(stoi(p_participantId)) == data_map_pointer->end()) {
             send_404 = true;
-            content.append("<h2> This page can only responde to GET requests </h2></div> </html>");
-            printf("Rejected! With: %s", http_header_word.c_str());
+            printf("ID %s wasn't found/wasn't a number", p_participantId.c_str());
+            content.append("<h2> We couldn't find a" + p_participantType + " with the ID " + p_participantId + " </h2></div> </html>");
+        } else {
+            content.append("<h1>" + p_participantType + ": " + p_participantId + "</h1>\n");
+            auto const &pair = *data_map_pointer->find(stoi(p_participantId));
+            content.append(
+                    "<table border=\"1\" cellspacing=\"0\"><thead><tr><th></th><th>Timestamp</th><th>Value</th></tr></thead>");
+            int index = 1;
+            for (auto &sec_pair : pair.second) {
+                content.append("<tr><td>" + std::to_string(index) + "</td><td>" + sec_pair.timestamp + "</td><td>" +
+                               std::to_string(sec_pair.value) + "</td></tr>\n");
+                index++;
+            }
+            content.append("</table>");
         }
-
-        // Write the document back to the client
-        std::ostringstream oss;
-        if (send_404)
-        {
-            oss << "HTTP/1.1 404 NOT FOUND\r\n";
-            content.insert(0, "<html><style type=\"text/css\"> body {background-color: #005792;color: #4caf50;font-family: monospace;} h1{font-size:15vw} h2{font-size: 5vw} div{margin-left: auto;margin-right: auto; width: 100em;text-align: center;}</style> <div><h1> 404 </h1> ");
-        }
-        else
-        {
-            oss << "HTTP/1.1 200 OK\r\n";
-        }
-        oss << "Cache-Control: no-cache, private\r\n";
-        oss << "Content-Type: text/html\r\n";
-        oss << "Content-Length: " << content.size() << "\r\n\r\n";
-        oss << content;
-
-        std::string output = oss.str();
-        int size = output.size() + 1;
-
-        send(new_socket, output.c_str(), size, 0);
-
-        //send(new_socket, hello, strlen(hello), 0);
-        printf("Hello message sent\n");
+    }
+    catch (std::exception &err) {
+        std::cout << "Conversion failure: " << err.what() << std::endl; // Note: what() tells the exact error
     }
 }
 
+void constructHTML404Method(){
+    send_404 = true;
+    content.append("<h2> This page can only responde to GET requests </h2></div> </html>");
+    printf("Rejected! With: %s", HTTP_method.c_str());
+}
+
+void constructHTML404SubDomain(){
+    send_404 = true;
+    content.append("<h2> We couldn't find a subsection named " + subDomain + " </h2></div> </html>");
+    printf("It was %s and not api", subDomain.c_str());
+}
+
+void sendRespons(){
+    // Write the document back to the client
+    std::ostringstream oss;
+    if (send_404) {
+        oss << "HTTP/1.1 404 NOT FOUND\r\n";
+        content.insert(0,
+                       "<html><style type=\"text/css\"> body {background-color: #005792;color: #4caf50;font-family: monospace;} h1{font-size:10vw} h2{font-size: 2vw} div{margin-left: auto;margin-right: auto; width: 100em;text-align: center;}</style> <div><h1> 404 </h1> ");
+    } else {
+        oss << "HTTP/1.1 200 OK\r\n";
+    }
+    oss << "Cache-Control: no-cache, private\r\n";
+    oss << "Content-Type: text/html\r\n";
+    oss << "Content-Length: " << content.size() << "\r\n\r\n";
+    oss << content;
+
+    std::string output = oss.str();
+    int size = output.size() + 1;
+
+    send(new_socket, output.c_str(), size, 0);
+}
--- server/udp_thread.cpp
+++ server/udp_thread.cpp
@@ -3,22 +3,22 @@
 //
 #include "globalVariables.h"
 
-int sockfd;
-char buffer[MAXLINE];
+int udpSockfd;
+char udpBuffer[MAXLINE];
 struct sockaddr_in servaddr, cliaddr;
 
-socklen_t sizeOfClientAdrr;
 int packetSize;
+socklen_t sizeOfClientAdrr;
 
 
-void setupSocket();
+void setupUdpSocket();
 void waitForPacket();
 void parseAndSavePacket();
 
 void udp_server()
 {
     printf("Starting UDP-Server...");
-    setupSocket();
+    setupUdpSocket();
 
     printf("UDP-Server is UP and running!");
     while (true)
@@ -36,17 +36,17 @@ void udp_server()
     }
 }
 
-void setupSocket(){
+void setupUdpSocket(){
 
     // Creating socket file descriptor
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    if ((udpSockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
 
     memset(&servaddr, 0, sizeof(servaddr));
-    memset(&cliaddr, 0, sizeof(cliaddr));
+    //memset(&cliaddr, 0, sizeof(cliaddr));
 
     // Filling server information
     servaddr.sin_family = AF_INET; // IPv4
@@ -54,7 +54,7 @@ void setupSocket(){
     servaddr.sin_port = htons(UDP_PORT);
 
     // Bind the socket with the server address
-    if (bind(sockfd, (const struct sockaddr *)&servaddr,
+    if (bind(udpSockfd, (const struct sockaddr *)&servaddr,
              sizeof(servaddr)) < 0)
     {
         perror("bind failed");
@@ -66,17 +66,17 @@ void setupSocket(){
 
 void waitForPacket(){
     // Wait for a message
-    packetSize = recvfrom(sockfd, (char *)buffer, MAXLINE,
+    packetSize = recvfrom(udpSockfd, (char *)udpBuffer, MAXLINE,
                           MSG_WAITALL,
                           (struct sockaddr *)&cliaddr,
                           &sizeOfClientAdrr);
-    buffer[packetSize] = '\0';
-    printf("Client sent us: %s\n", buffer);
+    udpBuffer[packetSize] = '\0';
+    printf("Client sent us: %s\n", udpBuffer);
 }
 
 void parseAndSavePacket(){
 
-    auto json_data = nlohmann::json::parse(buffer);
+    auto json_data = nlohmann::json::parse(udpBuffer);
 
     // Pointer points to corresponding vector
     if (json_data.at("type") == "c")
